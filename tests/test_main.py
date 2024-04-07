d5fdb237876b4bf178094907d2677fd38acce250
sanvida <sanvidatulam@gmail.com>
2024-04-07 18:39:40 +0530

    added test file

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")  # Make a request to the root endpoint
+    assert response.status_code == 200  # Check that the response status code is 200
+    assert response.json() == {"message": "Hello World"}  # Check the response content
